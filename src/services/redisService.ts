c53a0115d21c31e1cab4c213a5c263adab246ba4
bluecypher <s.sandeepshahi@gmail.com>
2025-02-05 11:15:56 +0530

    feat/pub-sub

--- src/services/redisService.ts
+++ src/services/redisService.ts
@@ -3,6 +3,7 @@ import { Redis } from "ioredis";
 
 class RedisService {
   private redis: Redis | null = null;
+  private subscriber: Redis | null = null;
 
   useDb(dbIndex: number): void {
     this.redis = InstanceManager.useDb(dbIndex);
@@ -12,6 +13,52 @@ class RedisService {
     if (!this.redis) {
       throw new Error("No Redis database selected. Call useDb(dbIndex) first.");
     }
+  }
+
+  getCurrentDb(): number | undefined {
+    this.checkInstance();
+    return this.redis!.options.db;
+  }
+
+  //publish changes to channel
+  private async publishChange(channel: string, key: string): Promise<void> {
+    this.checkInstance();
+    const dbIndex = this.redis!.options.db;
+    const message = JSON.stringify({ channel, dbIndex, key });
+
+    console.log(`Publishing: ${message}`);
+    await this.redis!.publish(`db-${dbIndex}`, message);
+  }
+
+  //subscribe
+  async subscribeToDb(
+    dbIndex: number,
+    callback: (message: any) => void
+  ): Promise<void> {
+    // if (!this.subscriber) {
+    //   throw new Error("Subscriber instance is not initialized.");
+    // }
+    if (!this.subscriber) {
+      this.subscriber = new Redis({
+        host: this.redis?.options.host,
+        port: this.redis?.options.port,
+        username: this.redis?.options.username,
+        password: this.redis?.options.password,
+      });
+    }
+
+    const channel = `db-${dbIndex}`;
+    this.subscriber.subscribe(channel, (err) => {
+      if (err) {
+        console.error(`Failed to subscribe to ${channel}:`, err);
+      } else {
+        console.log(`Subscribed to ${channel}`);
+      }
+    });
+
+    this.subscriber.on("message", (_, message) => {
+      callback(JSON.parse(message));
+    });
   }
 
   async setKey(
@@ -26,6 +73,7 @@ class RedisService {
       } else {
         await this.redis!.set(key, value);
       }
+      await this.publishChange("SET", key);
       return true;
     } catch {
       return false;
@@ -45,6 +93,7 @@ class RedisService {
     this.checkInstance();
     try {
       const result = await this.redis!.del(key);
+      if (result > 0) await this.publishChange("DELETE", key);
       return result > 0; // Returns true if the key was deleted
     } catch {
       return false;
@@ -70,44 +119,44 @@ class RedisService {
     }
   }
 
-  async addToSet(setKey: string, ...values: string[]): Promise<boolean> {
-    this.checkInstance();
-    try {
-      await this.redis!.sadd(setKey, ...values);
-      return true;
-    } catch {
-      return false;
-    }
-  }
+  // async addToSet(setKey: string, ...values: string[]): Promise<boolean> {
+  //   this.checkInstance();
+  //   try {
+  //     await this.redis!.sadd(setKey, ...values);
+  //     return true;
+  //   } catch {
+  //     return false;
+  //   }
+  // }
 
-  async getSetMembers(setKey: string): Promise<string[] | null> {
-    this.checkInstance();
-    try {
-      return this.redis!.smembers(setKey);
-    } catch {
-      return null;
-    }
-  }
+  // async getSetMembers(setKey: string): Promise<string[] | null> {
+  //   this.checkInstance();
+  //   try {
+  //     return this.redis!.smembers(setKey);
+  //   } catch {
+  //     return null;
+  //   }
+  // }
 
-  async removeFromSet(setKey: string, ...values: string[]): Promise<boolean> {
-    this.checkInstance();
-    try {
-      const result = await this.redis!.srem(setKey, ...values);
-      return result > 0; // Returns true if values were removed
-    } catch {
-      return false;
-    }
-  }
+  // async removeFromSet(setKey: string, ...values: string[]): Promise<boolean> {
+  //   this.checkInstance();
+  //   try {
+  //     const result = await this.redis!.srem(setKey, ...values);
+  //     return result > 0; // Returns true if values were removed
+  //   } catch {
+  //     return false;
+  //   }
+  // }
 
-  async setExists(setKey: string): Promise<boolean> {
-    this.checkInstance();
-    try {
-      const exists = await this.redis!.exists(setKey);
-      return exists > 0; // Returns true if the set exists
-    } catch {
-      return false;
-    }
-  }
+  // async setExists(setKey: string): Promise<boolean> {
+  //   this.checkInstance();
+  //   try {
+  //     const exists = await this.redis!.exists(setKey);
+  //     return exists > 0; // Returns true if the set exists
+  //   } catch {
+  //     return false;
+  //   }
+  // }
 }
 
 export default new RedisService();
